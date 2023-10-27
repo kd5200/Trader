43573739278e160d09eccf4aa71a6bf0a708112d
kd5200 <kareemdavis18@yahoo.com>
2023-10-27 17:15:06 -0400

    logic begins

--- alpaca.py
+++ alpaca.py
@@ -19,3 +19,8 @@ api = tradeapi.REST(API_KEY, API_SECRET, base_url=BASE_URL, api_version='v2')
 account = api.get_account()
 print(f"Account Status: {account.status}")
 print(f"Buying Power: {account.buying_power}")
+
+symbol = 'AAPL'
+qty = 1
+side = 'buy'  # 'buy' or 'sell'
+order_type = 'market'  # 'market', 'limit', 'stop', etc.
